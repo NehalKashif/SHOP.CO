b618ea76af1f0084a8ee532419b0c87f18f909cd
Nehal Kashif <nehalkashif112@gmail.com>
2025-08-01 00:13:41 +0500

    Landing Page Responsiveness

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let tag = document.querySelector(".tag")
 closeBtn.addEventListener("click", () => {
     tag.classList.add("hidden");
 });
+
+
+let menu = document.querySelector(".menu")
+let dropdown = document.querySelector(".dropdown")
+let close = document.querySelector(".closeNav")
+
+menu.addEventListener("click", () => {
+    dropdown.classList.remove("top-[-575%]")
+    dropdown.classList.add("top-[20%]")
+})
+close.addEventListener("click", () => {
+    dropdown.classList.remove("top-[20%]")
+    dropdown.classList.add("top-[-575%]")
+})
