d72e659fbbb41e24f95cf531228c00a7a59e7f9d
Nehal Kashif <nehalkashif112@gmail.com>
2025-08-07 23:08:21 +0500

    Final Change

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let dropdown = document.querySelector(".dropdown")
 let close = document.querySelector(".closeNav")
 
 menu.addEventListener("click", () => {
-    dropdown.classList.remove("top-[-470%]")
+    dropdown.classList.remove("top-[-575%]")
     dropdown.classList.add("top-[30%]")
 })
 close.addEventListener("click", () => {
 
     dropdown.classList.remove("top-[30%]")
-    dropdown.classList.add("top-[-470%]")
+    dropdown.classList.add("top-[-575%]")
 })
 
 
