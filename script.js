ae6174f1f59084d16247c3702e3eb82defd46334
Nehal Kashif <nehalkashif112@gmail.com>
2025-08-07 22:54:24 +0500

    Final Website

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-let closeBtn = document.querySelector(".close")
-let tag = document.querySelector(".tag")
-
-closeBtn.addEventListener("click", () => {
-    tag.classList.add("hidden");
-});
-
-
 let menu = document.querySelector(".menu")
 let dropdown = document.querySelector(".dropdown")
 let close = document.querySelector(".closeNav")
 
 menu.addEventListener("click", () => {
-    dropdown.classList.remove("top-[-575%]")
-    dropdown.classList.add("top-[20%]")
+    dropdown.classList.remove("top-[-470%]")
+    dropdown.classList.add("top-[30%]")
 })
 close.addEventListener("click", () => {
-    dropdown.classList.remove("top-[20%]")
-    dropdown.classList.add("top-[-575%]")
+
+    dropdown.classList.remove("top-[30%]")
+    dropdown.classList.add("top-[-470%]")
 })
 
 
