3ddd1f99a46bebe7f7dad5747f38f17eff527219
Nehal Kashif <nehalkashif112@gmail.com>
2025-08-16 22:33:39 +0500

    sales js file

---
+++ sales.js
@@ -0,0 +1,62 @@
+let menu = document.querySelector(".menu")
+let dropdown = document.querySelector(".dropdown")
+let close = document.querySelector(".closeNav")
+
+menu.addEventListener("click", () => {
+    dropdown.classList.remove("top-[-575%]")
+    dropdown.classList.add("top-[30%]")
+})
+close.addEventListener("click", () => {
+
+    dropdown.classList.remove("top-[30%]")
+    dropdown.classList.add("top-[-575%]")
+})
+
+
+let view = document.querySelector(".view")
+    
+view.addEventListener("click", () => {
+    let hide = document.querySelectorAll(".hide")
+    if(view.innerText === "View All"){
+        hide.forEach(element => {
+            element.classList.remove("hidden")
+            view.innerText ="Close"
+        });
+    }else{
+        hide.forEach(element => {
+            element.classList.add("hidden")
+            view.innerText ="View All"
+        })
+    }
+})
+
+
+let topview = document.querySelector(".topview")
+    
+topview.addEventListener("click", () => {
+    let hide = document.querySelectorAll(".tophide")
+    if(topview.innerText === "View All"){
+        hide.forEach(element => {
+            element.classList.remove("hidden")
+            topview.innerText ="Close"
+        });
+    }else{
+        hide.forEach(element => {
+            element.classList.add("hidden")
+            topview.innerText ="View All"
+        })
+    }
+})
+
+let dd = document.querySelector(".dd")
+let list = document.querySelector(".list")
+
+dd.addEventListener("mouseenter", ()=>{
+    list.style.backgroundColor = 'black';
+    list.style.color = 'white';
+})
+
+dd.addEventListener("mouseleave", ()=>{
+    list.style.backgroundColor = 'white';
+    list.style.color = 'black';
+})
