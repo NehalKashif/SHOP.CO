ec4db15f33dc7db265053fdaf2c53e2a93cf3b2c
Nehal Kashif <nehalkashif112@gmail.com>
2025-08-24 18:39:25 +0500

    Cart and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["*.html", ".js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeRight: {
+          '0%': { opacity: '0', transform: 'translateX(-50px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeUp: {
+          '0%': { opacity: '0', transform: 'translateY(50px)' },
+          '100%': { opacity: '1', transform: 'translateY(0px)'},
+        },
+      },
+      animation: {
+        fadeRight: 'fadeRight 1s ease-out forwards',
+        fadeUp: 'fadeUp 1s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
